526e14242e14849cfc586bfe7f27dfbfcd3c365b
Andrii Gubanov <andrey.a.gubanov@gmail.com>
2024-04-01 11:55:29 +0200

    feat: Algolia

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ import { themes as prismThemes } from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  plugins: [require.resolve('docusaurus-lunr-search')],
+  // plugins: [require.resolve('docusaurus-lunr-search')],
   title: 'Vovk.ts',
   tagline: 'Structural add-on for Next.js',
   favicon: 'img/favicon.ico',
@@ -142,6 +142,35 @@ const config = {
       prism: {
         theme: prismThemes.nightOwl,
         darkTheme: prismThemes.dracula,
+      },
+      algolia: {
+        // The application ID provided by Algolia
+        appId: 'BR2W6LNZLW',
+  
+        // Public API key: it is safe to commit it
+        apiKey: 'f0da4340b2cec8d8a28ef56e7bb84182',
+  
+        indexName: 'vovk',
+  
+        // Optional: see doc section below
+        contextualSearch: true,
+  
+        // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+        // externalUrlRegex: 'external\\.com|domain\\.com',
+  
+        // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+        /* replaceSearchResultPathname: {
+          from: '/docs/', // or as RegExp: /\/docs\//
+          to: '/',
+        }, */
+  
+        // Optional: Algolia search parameters
+        searchParameters: {},
+  
+        // Optional: path for search page that enabled by default (`false` to disable it)
+        searchPagePath: 'search',
+  
+        //... other Algolia params
       },
     }),
 };
